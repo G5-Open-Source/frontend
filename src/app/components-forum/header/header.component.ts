2bd87275cdce9bdbfe7cb0965b56342e85eb8536
NotSeijas <notcheatseijas@gmail.com>
2025-05-16 01:45:35 +0100

    UPDATE: all color login-register-foroms

--- src/app/components-forum/header/header.component.ts
+++ src/app/components-forum/header/header.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { NgFor} from '@angular/common';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [NgFor],
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
   navItems = ['Foro', 'Proyectos', 'Invitaciones', 'Notificaciones'];
@@ -14,7 +14,7 @@ export class HeaderComponent {
   user = {
     username: 'Adriana',
     role: 'Moderador',
-    avatarUrl: 'assets/images/user-avatar.jpg'
+    avatarUrl: 'https://cdn-icons-png.flaticon.com/512/3532/3532704.png',
   };
 
   setActiveItem(item: string): void {
